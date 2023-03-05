51da94ddfc64eeca16286097e44a62672df17f44
jinhaiyang001 <jinhaiyang001@ke.com>
2023-03-05 16:19:29 +0800

    设置路由

--- testRoute/src/main/java/com/jhy/testRoute/controller/TestController.java
+++ testRoute/src/main/java/com/jhy/testRoute/controller/TestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    @GetMapping("/test/hello")
+    @GetMapping("/testRoute/hello")
     public String testHello(){
         return "后端服务返回数据";
     }
